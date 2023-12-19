e90dc5ead15c0f5277a52b7934ad04226e199ca4
Sascha Kiebler <sascha.kiebler@htwg-konstanz.de>
2023-12-19 23:19:59 +0100

    Add Conversation reference to Message class

---
+++ src/main/java/de/saschakiebler/model/Conversation.java
@@ -0,0 +1,67 @@
+package de.saschakiebler.model;
+
+
+import java.util.List;
+
+import java.time.LocalDateTime;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+/**
+ * Represents a conversation in the chat application.
+ * A conversation has a list of messages, a timestamp, and a name.
+ */
+@Entity
+public class Conversation extends PanacheEntity {
+    
+
+@OneToMany(mappedBy = "conversation")
+    public List<Message> messages;
+
+    @Column
+    public LocalDateTime timestamp;
+
+    @Column
+    public String name;
+
+    public Conversation() {
+    }
+
+    public Conversation(String name) {
+        this.name = name;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public void addMessage(Message message) {
+        this.messages.add(message);
+    }
+
+    public void removeMessage(Message message) {
+        this.messages.remove(message);
+    }
+
+    public List<Message> getMessages() {
+        return this.messages;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return this.timestamp;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+
+}
--- src/main/java/de/saschakiebler/model/Message.java
+++ src/main/java/de/saschakiebler/model/Message.java
@@ -5,54 +5,144 @@ import java.time.LocalDateTime;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 
+/**
+ * This class represents a message in the database.
+ * A message has a sender, a text, a timestamp, and a conversation.
+ * The conversation is a reference to the conversation in which the message was sent.
+ * A message can have different senders, which are represented by the MessageRoles enum.
+ */
 @Entity
-public class Message extends PanacheEntity{
+public class Message extends PanacheEntity {
 
+    /**
+     * The sender of the message.
+     */
     @Column(length = 40)
     public String sender;
 
+    /**
+     * The text content of the message.
+     */
     @Column(length = 4000)
     public String text;
 
+    /**
+     * The timestamp when the message was sent.
+     */
     @Column
     public LocalDateTime timestamp;
 
-    // Constructors
+    /**
+     * The conversation in which the message was sent.
+     */
+    @ManyToOne
+    public Conversation conversation;
+
+    /**
+     * Default constructor.
+     */
     public Message() {
-        // Empty constructor
+    
     }
 
+    /**
+     * Constructor with sender and text parameters.
+     * 
+     * @param sender The sender of the message.
+     * @param text The text content of the message.
+     */
     public Message(String sender, String text) {
         this.sender = sender;
         this.text = text;
-        this.timestamp = LocalDateTime.now(); // Automatically set the timestamp when the message is created
+        this.timestamp = LocalDateTime.now(); 
     }
 
-    // Getters
+    /**
+     * Constructor with sender, text, and conversation parameters.
+     * 
+     * @param sender The sender of the message.
+     * @param text The text content of the message.
+     * @param conversation The conversation in which the message was sent.
+     */
+    public Message(String sender, String text, Conversation conversation) {
+        this.sender = sender;
+        this.text = text;
+        this.timestamp = LocalDateTime.now(); 
+        this.conversation = conversation;
+    }
+
+    /**
+     * Get the sender of the message.
+     * 
+     * @return The sender of the message.
+     */
     public String getSender() {
         return sender;
     }
 
+    /**
+     * Get the text content of the message.
+     * 
+     * @return The text content of the message.
+     */
     public String getText() {
         return text;
     }
 
+    /**
+     * Get the timestamp when the message was sent.
+     * 
+     * @return The timestamp when the message was sent.
+     */
     public LocalDateTime getTimestamp() {
         return timestamp;
     }
 
-    // Setters
+    /**
+     * Get the conversation in which the message was sent.
+     * 
+     * @return The conversation in which the message was sent.
+     */
+    public Conversation getConversation() {
+        return conversation;
+    }
+
+    /**
+     * Set the sender of the message.
+     * 
+     * @param sender The sender of the message.
+     */
     public void setSender(String sender) {
         this.sender = sender;
     }
 
+    /**
+     * Set the text content of the message.
+     * 
+     * @param text The text content of the message.
+     */
     public void setText(String text) {
         this.text = text;
     }
 
+    /**
+     * Set the timestamp when the message was sent.
+     * 
+     * @param timestamp The timestamp when the message was sent.
+     */
     public void setTimestamp(LocalDateTime timestamp) {
         this.timestamp = timestamp;
+    }
+
+    /**
+     * Set the conversation in which the message was sent.
+     * 
+     * @param conversation The conversation in which the message was sent.
+     */
+    public void setConversation(Conversation conversation) {
+        this.conversation = conversation;
     }
 }
 
